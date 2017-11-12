106cc166005525db39d77648208f21fa2d95cd02
Martin <m4rtin.t@gmail.com>
2017-11-11 17:51:32 -0800

    Flashing Light Script and prefab

---
+++ Assets/Scripts/FlashingLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour {
+
+	float lightsOnDur = 0.05f;
+	float lightsOffDur = 2f;
+	bool isLightOn = false;
+	public float timer;
+	Light lightBulb;
+
+	void Start() {
+		timer = lightsOffDur;
+		lightBulb = gameObject.GetComponent<Light> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0) {
+			setTimer ();
+			timer = isLightOn? lightsOnDur: lightsOffDur;
+
+			switchLights ();
+		}	
+	}
+
+	void setTimer() {
+		//This timer has to change depening if it's lights on/off
+		lightsOffDur = Random.Range(0.05f, 2f);
+		lightsOnDur = Random.Range (0.05f, 0.5f);
+	}
+
+	void switchLights() {
+		if (isLightOn)
+			lightBulb.intensity = 5;
+		else
+			lightBulb.intensity = 0;
+
+		isLightOn = !isLightOn;
+	}
+}
