2ef31b6df4d3ae5ecc684740ea2177946a19f8db
qwartzclock <ontograsstown@gmail.com>
2017-11-23 16:40:45 -0800

    Enemy controller script and base behaviour script
    Enemy controller launches waves of attacks. Enemies can be summoned with a delay. After all enemies in a wave are defeated, next wave begins. This continues until all waves are done. There is no failure or victory state yet.
    
    Base behavior has room for a delay, then an attack that you must implement yourself. An (very *very* basic) example is provided.
    All enemies must extend EnemyBehaviorScript.

---
+++ Assets/Scripts/MonsterScripts/MrMonsterDoodScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrMonsterDoodScript : EnemyBehaviorScript {
+
+	public override void launchAttack (){
+		print("'sup, I'm Mr Monster Dood. " + gameObject.name);
+	}
+
+}
