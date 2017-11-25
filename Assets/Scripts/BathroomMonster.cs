f99bc14fcfe546833a0fdc4478aac1da1cde0d96
Martin Tsang <m4rtin.t@gmail.com>
2017-11-24 23:25:19 -0800

    Setting up door closing game mechanic

---
+++ Assets/Scripts/BathroomMonster.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathroomMonster : MonoBehaviour {
+
+    enum MonsterStage
+    {
+        DoorOpen,
+        DoorCloseChance,
+        DoorClosed,
+        UserFailed
+    }
+
+    //Current stage
+    MonsterStage currentStage = MonsterStage.DoorOpen;
+
+    //Bathroom Door
+    GameObject doorHinge;
+    doorOpen door;
+
+    //Open Door
+    public float doorAngle;
+    float doorOpenDuration;
+
+    //Open more Door
+    public float loseDoorAngle;
+
+    //Seconds for user to close the door
+    float secondsBeforeUserLose;
+
+    //Seconds before jump scare starts
+    float secondsBeforeJumpScareStarts;
+
+    void Awake()
+    { 
+        //Door
+        doorHinge = GameObject.Find("DoorHinge");
+        door = doorHinge.GetComponent<doorOpen>();
+        doorOpenDuration = Random.Range(3, 5);
+
+        //Seconds for user to close the door
+        secondsBeforeUserLose = Random.Range(5,8);
+
+        //Seconds before jump scare starts
+        secondsBeforeJumpScareStarts = Random.Range(5,10);
+    }
+
+    // Use this for initialization
+    void Start () {
+        door.setAngleDur(doorAngle, doorOpenDuration);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch(currentStage)
+        {
+            case MonsterStage.DoorOpen:
+                finishedOpenDoor();
+                break;
+            case MonsterStage.DoorCloseChance:
+                doorClosingChance();
+                break;
+            case MonsterStage.DoorClosed:
+                userPassed();
+                break;
+            case MonsterStage.UserFailed:
+                userFailed();
+                break;
+        }
+		
+	}
+    
+    void finishedOpenDoor()
+    {
+        int currentAngle = (int)doorHinge.transform.localRotation.eulerAngles.y;
+        if (currentAngle == (int)doorAngle)
+        {
+            currentStage = MonsterStage.DoorCloseChance;
+        }
+    }
+
+
+    void doorClosingChance()
+    {
+        secondsBeforeUserLose -= Time.deltaTime;
+        if (secondsBeforeUserLose < 0)
+        {
+            currentStage = MonsterStage.UserFailed;
+            door.setAngleDur(loseDoorAngle, doorOpenDuration);
+        }
+
+        //Check if Door is closed
+        if (doorHinge.transform.rotation.y == 0)
+        {
+            currentStage = MonsterStage.DoorClosed;
+        }
+    }
+
+    void userPassed()
+    {
+        Destroy(gameObject);
+    }
+
+    void userFailed()
+    {
+        secondsBeforeJumpScareStarts -= Time.deltaTime;
+        if (secondsBeforeJumpScareStarts < 0)
+        {
+            GameObject.Find("CameraObject").GetComponent<JumpScare>().startJumpScare();
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/doorOpen.cs
+++ Assets/Scripts/doorOpen.cs
@@ -7,38 +7,12 @@ public class doorOpen : MonoBehaviour {
 
 	public float rotationAngle;
 	public float rotationalSpeed;
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.A)) {
-			setAngleDur (90f, 2f);
-		} 
-		if (Input.GetKeyDown (KeyCode.D)) {
-			setAngleDur (0f, 0.2f, Ease.InSine);
-		}
-
-		if (Input.GetKeyDown (KeyCode.Q)) {
-			setAngleSpeed (90f, 45f);
-		} 
-
-		if (Input.GetKeyDown (KeyCode.E)) {
-			setAngleSpeed (0f, 450f, Ease.InSine);
-		}
-
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			setAngleDur(rotationAngle, rotationalSpeed);
-		}
-
-	}
 
 	//Tween with a set duration
 	public void setAngleDur(float angle, float duration, Ease easey = Ease.OutSine){
 		//rotationAngle = angle;
 		gameObject.transform.DOKill();
 		gameObject.transform.DORotate (new Vector3 (0.0f, angle, 0.0f), duration).SetEase(easey);
-
-		Debug.Log (gameObject.transform.localEulerAngles.y);
-
 	}
 
 	//Tween with speed in deg per second
@@ -46,10 +20,12 @@ public class doorOpen : MonoBehaviour {
 		//rotationAngle = angle;
 		gameObject.transform.DOKill();
 		gameObject.transform.DORotate (new Vector3 (0.0f, angle, 0.0f), Mathf.Abs(gameObject.transform.localEulerAngles.y-angle)/speed).SetEase(easey);
-
-		Debug.Log (gameObject.transform.localEulerAngles.y);
-
 	}
+
+    public void glitchToCertainAngle(float angle, float speed)
+    {
+        
+    }
 
 
 }
