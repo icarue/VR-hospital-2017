8ba9d02fe4640b374e1749eea4fc47203d7c6b70
Martin Tsang <m4rtin.t@gmail.com>
2017-11-26 22:18:54 -0800

    saving the project settings

---
+++ Assets/Scripts/CorriderMonster/CorridorMonster.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorridorMonster : MonoBehaviour {
+
+    enum MonsterStages
+    {
+        MoveIn,
+        UserInteraction,
+        DoorOpen,
+        JumpScare,
+        UserWin
+    }
+
+    MonsterStages currentStage = MonsterStages.MoveIn;
+
+    //User interaction
+    float timeForCurtainsToStayOpen;
+
+    private void Awake()
+    {
+        timeForCurtainsToStayOpen = Random.Range(5, 8);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        switch (currentStage)
+        {
+            case MonsterStages.MoveIn:
+                monsterMoveIntoPosition();
+                break;
+            case MonsterStages.UserInteraction:
+                userInteraction();
+                break;
+            case MonsterStages.DoorOpen:
+                break;
+            case MonsterStages.JumpScare:
+                break;
+            case MonsterStages.UserWin:
+                break;
+        }
+	}
+
+    void monsterMoveIntoPosition()
+    {
+        currentStage = MonsterStages.UserInteraction;
+    }
+
+    void userInteraction()
+    {
+        timeForCurtainsToStayOpen -= Time.deltaTime;
+
+    }
+}
