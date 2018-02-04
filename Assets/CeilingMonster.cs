e31b53047f733d3888957f4beeb06de44e880f45
Martin Tsang <m4rtin.t@gmail.com>
2018-02-04 01:03:32 -0800

    Ceiling monster initialize

---
+++ Assets/CeilingMonster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingMonster : Monster {
+
+    enum MonsterStages
+    {
+
+    }
+
+    protected override void setTimeUntilJumpScare()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void setupMonsterToStartAttack()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
