1fd893d5f3412bcd55af9a676e74bcbd9964a955
Martin Tsang <m4rtin.t@gmail.com>
2018-03-04 19:16:07 -0800

    Bed Side lamp on click

---
+++ Assets/BedSideLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedSideLight : MonoBehaviour {
+
+    public bool isLightOn { get; private set; }
+    [SerializeField]
+    GameObject spotLight;
+
+    private void OnMouseDown()
+    {
+        spotLight.SetActive(isLightOn);
+        isLightOn = !isLightOn;
+    }
+}
